33f11476aa50d14c85fbb0eea7bd9b6f19d0a6c6
FelipeSilvaPortifolio <portifoliofelipesilva@gmail.com>
2025-02-22 21:20:30 -0300

    Implement end-to-end checkout flow and update login credentials

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,45 @@
 /// <reference types="cypress" />
+import produtosPage from "../support/page_objects/produtos.page";
+import fazCheckoutPage from "../support/page_objects/fazCheckout.page";
+import { faker } from "@faker-js/faker";
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+context("Exercicio - Testes End-to-end - Fluxo de pedido", () => {
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final 
+  */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    beforeEach(() => {
+        cy.visit("produtos/");
+    
+    });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    it("Deve fazer um pedido na loja Ebac Shop de ponta a ponta", function () {
+        const quantidadeProdutos = 4; // Define a quantidade de produtos conforme o cenário
+        const nome = faker.person.firstName();
+        const sobrenome = faker.person.lastName();
+        const email = faker.internet.email(nome, sobrenome);
+        const senha = faker.internet.password();
 
+    // Adiciona os produtos ao carrinho
+    cy.fixture('produtos').then(dados => {
+        dados.slice(0, quantidadeProdutos).forEach(produto => {
+            produtosPage.buscarProduto(produto.nomeProduto);
+            produtosPage.addProdutoCarrinho(produto.tamanho, produto.cor, produto.quantidade);
+            
+        });
+    });
 
-})
+    // Realiza o checkout
+    cy.fixture('checkout').then(dados => {
+        fazCheckoutPage.checkout(nome, sobrenome, dados.empresa, dados.pais, dados.endereco, dados.complemento, dados.cidade, dados.estado, dados.cep, dados.telefone, email, senha, dados.comentario);
+    });
+    
+    // Valida se o pedido foi concluído com sucesso
+    cy.get('.page-title').should('be.visible').and('contain', 'Pedido recebido');
+    });
+});
---
+++ cypress/support/page_objects/fazCheckout.page.js
@@ -0,0 +1,33 @@
+class checkoutPage {
+
+    checkout(nome, sobrenome, empresa, pais, endereco, complemento, cidade, estado, cep, telefone, email, senha, comentario) {
+        //ações do método
+
+        cy.get('#cart > .dropdown-toggle').click();
+        cy.get('.checkout').eq(1).click();
+        cy.get('#billing_first_name').type(nome);
+        cy.get('#billing_last_name').type(sobrenome);
+        cy.get('#billing_company').type(empresa);
+        cy.get('#select2-billing_country-container').click();
+        cy.get('.select2-search__field').type(pais + '{enter}');
+        cy.get('#billing_address_1').type(endereco);
+        cy.get('#billing_address_2').type(complemento);
+        cy.get('#billing_city').type(cidade);
+        cy.get('#select2-billing_state-container').click();
+        cy.get('.select2-search__field').type(estado + '{enter}');
+        cy.get('#billing_postcode').type(cep);
+        cy.get('#billing_phone').type(telefone);
+        cy.get('#billing_email').type(email);
+        cy.get('#createaccount').check();
+        cy.get('#account_password').type(senha);
+        cy.get('#order_comments').type(comentario);
+        cy.get('#payment_method_cheque').check();
+        cy.get('#terms').check();
+        cy.get('#place_order').click();
+
+
+    }
+
+}
+
+export default new checkoutPage()
